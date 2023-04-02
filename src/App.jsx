c425c02ad0ec8c019f49b349c0ef498cec1a71c8
Abel Emmanuel <85368219+abellmanuell@users.noreply.github.com>
2023-04-02 23:17:48 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Chatroom from "./components/Chatroom";
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Registration />} />
